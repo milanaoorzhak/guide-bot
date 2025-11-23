d9b74a0b6e010e42d76d681e05fa037b0438d42b
milanaoorzhak <milanaoor@gmail.com>
2025-11-23 21:22:05 +0700

    Добавлен базовый функционал и gitignore

---
+++ src/GuideBot/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Добро пожаловать!");
+
+while (true)
+{
+    Console.WriteLine("\nВведите одну из команд для дальнейшей работы: /start, /help, /info, /exit.");
+
+    string? userName;
+    var userCommand = Console.ReadLine();
+
+    switch (userCommand)
+    {
+        case "/start":
+            Console.Write("Введите, пожалуйста, ваше имя: ");
+            userName = Console.ReadLine();
+            break;
+        case "/help":
+            Console.WriteLine("\nДля работы с программой необходимо ввести одну из комманд: \n/start - для старта работы программы, \n/help - отображает краткую справочную информацию о том, как пользоваться программой, \n/info - предоставляет информацию о версии программы и дате её создания, \n/exit - для выхода из программы");
+            break;
+        case "/info":
+            Console.WriteLine("Dерсия программы 1.0\nДата создания 23.11.2025");
+            break;
+        case "/echo": break;
+        case "/exit": return;
+    }
+}
