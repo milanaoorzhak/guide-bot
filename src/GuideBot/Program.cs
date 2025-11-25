e63d874e96dbffb7a139a0e677c2a002faaf275e
milanaoorzhak <milanaoor@gmail.com>
2025-11-25 11:24:14 +0700

    loop updated

--- src/GuideBot/Program.cs
+++ src/GuideBot/Program.cs
@@ -1,20 +1,19 @@
 ﻿string? userName = string.Empty;
+string welcomeText = "Добро пожаловать!";
+string menuText = "\nВведите одну из команд для дальнейшей работы: /start, /help, /info, /exit.";
+string helpCommandText = "Для работы с программой необходимо ввести одну из комманд: \n/start - для старта работы программы, \n/help - отображает краткую справочную информацию о том, как пользоваться программой, \n/info - предоставляет информацию о версии программы и дате её создания, \n/echo - данная команда становится доступной после ввода имени. При вводе этой команды с некоторым значением (например, /echo Hello), программа возвращает введенный текст (\"Hello\")\n/exit - для выхода из программы";
+string infoCommandText = "Dерсия программы 1.0\nДата создания 23.11.2025";
+string echoCommandText = "Имя пользователя не введено! Для работы с командой введите имя пользователя с помошью вызова команды /start";
+bool isExit = false;
 
-Console.WriteLine("Добро пожаловать!");
+Console.WriteLine(welcomeText);
 
-while (true)
+while (!isExit)
 {
-    if (!string.IsNullOrEmpty(userName))
-    {
-        Console.WriteLine($"\n{userName}, введите одну из команд для дальнейшей работы: /start, /help, /info, /echo, /exit.");
-    }
-    else
-    {
-        Console.WriteLine("\nВведите одну из команд для дальнейшей работы: /start, /help, /info, /exit.");
-    }
+    Console.WriteLine(!string.IsNullOrWhiteSpace(userName) ? $"\n{userName}, {menuText}" : menuText);
 
     var userCommand = Console.ReadLine();
-    if (!string.IsNullOrEmpty(userCommand))
+    if (!string.IsNullOrWhiteSpace(userCommand))
     {
         switch (userCommand!.Trim())
         {
@@ -22,24 +21,19 @@ while (true)
                 userName = GetUserName();
                 break;
             case "/help":
-                if (!string.IsNullOrEmpty(userName)) Console.WriteLine($"{userName}!");
-                Console.WriteLine("Для работы с программой необходимо ввести одну из комманд: \n/start - для старта работы программы, \n/help - отображает краткую справочную информацию о том, как пользоваться программой, \n/info - предоставляет информацию о версии программы и дате её создания, \n/echo - данная команда становится доступной после ввода имени. При вводе этой команды с некоторым значением (например, /echo Hello), программа возвращает введенный текст (\"Hello\")\n/exit - для выхода из программы");
+                PrintUserName();
+                Console.WriteLine(helpCommandText);
                 break;
             case "/info":
-                if (!string.IsNullOrEmpty(userName)) Console.WriteLine($"{userName}!");
-                Console.WriteLine("Dерсия программы 1.0\nДата создания 23.11.2025");
+                PrintUserName();
+                Console.WriteLine(infoCommandText);
                 break;
             case string echoInput when echoInput.Contains("/echo") && echoInput.Length > 6:
-                if (string.IsNullOrEmpty(userName))
-                {
-                    Console.WriteLine($"Имя пользователя не введено! Для работы с командой введите имя пользователя с помошью вызова команды /start");
-                }
-                else
-                {
-                    Console.WriteLine(echoInput.Substring(6));
-                }
+                Console.WriteLine(string.IsNullOrWhiteSpace(userName) ? echoCommandText : echoInput.Substring(6));
                 break;
-            case "/exit": return;
+            case "/exit":
+                isExit = true;
+                break;
         }
     }
 }
@@ -52,7 +46,12 @@ string GetUserName()
         Console.Write("Введите, пожалуйста, ваше имя: ");
         name = Console.ReadLine();
     }
-    while (string.IsNullOrEmpty(name));
+    while (string.IsNullOrWhiteSpace(name));
 
     return name;
+}
+
+void PrintUserName()
+{
+    if (!string.IsNullOrWhiteSpace(userName)) Console.WriteLine($"{userName}!");
 }
