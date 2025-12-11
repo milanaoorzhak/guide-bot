4b15c7519ab025d2a741f4ba119e34e488893a0b
milanaoorzhak <milanaoor@gmail.com>
2025-12-11 19:33:17 +0700

    try/catch moved

--- src/GuideBot/Program.cs
+++ src/GuideBot/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System.ComponentModel;
 using GuideBot;
 
+int taskCount;
+int taskLength;
+int min = 0;
+int max = 100;
+string? userName = string.Empty;
+string welcomeText = "Добро пожаловать!";
+string menuText = "\nДоступные команды: /start, /help, /info, /echo, /addtask, /showtasks, /removetask, /exit";
+bool isExit = false;
+List<string> tasks = new();
+
+Console.Write(welcomeText);
 try
 {
-    int taskCount;
-    int taskLength;
-    int min = 0;
-    int max = 100;
-    string? userName = string.Empty;
-    string welcomeText = "Добро пожаловать!";
-    string menuText = "\nДоступные команды: /start, /help, /info, /echo, /addtask, /showtasks, /removetask, /exit";
-    bool isExit = false;
-    List<string> tasks = new();
-
-    Console.Write(welcomeText);
-
     Console.Write("Введите максимально допустимое количество задач: ");
     var taskCountInput = Console.ReadLine();
     taskCount = ParseAndValidateInt(taskCountInput, min, max);
@@ -65,140 +64,6 @@ try
             }
         }
     }
-
-    void Start()
-    {
-        userName = GetUserName();
-        Console.WriteLine($"Привет, {userName}! Чем могу помочь?");
-    }
-
-    string GetUserName()
-    {
-        string? name;
-        do
-        {
-            Console.Write("Пожалуйста, введите ваше имя:");
-            name = Console.ReadLine();
-        }
-        while (string.IsNullOrWhiteSpace(name));
-
-        return name;
-    }
-
-    void PrintUserName()
-    {
-        if (!string.IsNullOrWhiteSpace(userName)) Console.WriteLine($"{userName}!");
-    }
-
-    void Help()
-    {
-        string helpCommandText = "Для работы с программой необходимо ввести одну из комманд: \n/start - для старта работы программы, \n/help - отображает краткую справочную информацию о том, как пользоваться программой, \n/info - предоставляет информацию о версии программы и дате её создания, \n/echo - данная команда становится доступной после ввода имени. При вводе этой команды с некоторым значением (например, /echo Hello), программа возвращает введенный текст (\"Hello\")\n/addtask - добавляет задачу в список\n/showtasks - отображает список всех добавленных задач\n/removetask - удаляет задачу по номеру в списке\n/exit - для выхода из программы";
-        Console.WriteLine(helpCommandText);
-    }
-
-    void Info()
-    {
-        string infoCommandText = "Dерсия программы 1.0\nДата создания 23.11.2025";
-        Console.WriteLine(infoCommandText);
-    }
-
-    void Echo(string echoInput)
-    {
-        string echoCommandText = "Имя пользователя не введено! Для работы с командой введите имя пользователя с помошью вызова команды /start";
-        Console.WriteLine(string.IsNullOrWhiteSpace(userName) ? echoCommandText : echoInput.Substring(6));
-    }
-
-    void CheckTaskCount()
-    {
-        if (tasks.Any() && tasks.Count == taskCount) throw new TaskCountLimitException(taskCount);
-    }
-
-    void AddTask()
-    {
-        Console.Write("Пожалуйста, введите описание задачи: ");
-        var task = Console.ReadLine();
-
-        if (IsValidTaskLength(task) && !IsDuplicateTask(task))
-        {
-            tasks.Add(task!);
-            Console.WriteLine($"Задача \"{task}\" добавлена.");
-        }
-    }
-
-    bool IsValidTaskLength(string? task)
-    {
-        ValidateString(task);
-
-        if (task!.Length > taskLength) throw new TaskLengthLimitException(task.Length, taskLength);
-
-        return true;
-    }
-
-    bool IsDuplicateTask(string task)
-    {
-        foreach (var t in tasks)
-        {
-            if (task.Equals(t)) throw new DuplicateTaskException(task);
-        }
-
-        return false;
-    }
-
-    void ShowTasks()
-    {
-        if (tasks.Any())
-        {
-            Console.WriteLine("Вот ваш список задач:");
-            for (var i = 1; i <= tasks.Count(); i++)
-            {
-                Console.WriteLine($"{i} - {tasks[i - 1]}");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Список задач пуст");
-        }
-    }
-
-    void RemoveTask()
-    {
-        if (tasks.Any())
-        {
-            ShowTasks();
-            Console.Write($"Введите номер задачи для удаления: ");
-            var taskNumber = Console.ReadLine();
-            if (int.TryParse(taskNumber, out int result) && result > 0 && result <= tasks.Count())
-            {
-                tasks.Remove(tasks[result - 1]);
-                Console.WriteLine($"Задача удалена.");
-            }
-            else
-            {
-                Console.WriteLine($"Введен неверный номер для удаления");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Список задач пуст");
-        }
-    }
-
-    int ParseAndValidateInt(string? str, int min, int max)
-    {
-        if (!(int.TryParse(str, out int number) && number >= min && number <= max)) throw new ArgumentException($"Это должно быть число от {min} до {max}");
-
-        return number;
-    }
-
-    void ValidateString(string? str)
-    {
-        if (string.IsNullOrWhiteSpace(str)) throw new ArgumentException($"Строка не должна быть пустой");
-    }
-
-    void Exit()
-    {
-        isExit = true;
-    }
 }
 catch (ArgumentException ex)
 {
@@ -219,4 +84,138 @@ catch (DuplicateTaskException ex)
 catch (Exception ex)
 {
     Console.WriteLine($"Произошла непредвиденная ошибка: {ex.GetType()}, {ex.Message}, {ex.StackTrace}, {ex.InnerException}");
+}
+
+void Start()
+{
+    userName = GetUserName();
+    Console.WriteLine($"Привет, {userName}! Чем могу помочь?");
+}
+
+string GetUserName()
+{
+    string? name;
+    do
+    {
+        Console.Write("Пожалуйста, введите ваше имя:");
+        name = Console.ReadLine();
+    }
+    while (string.IsNullOrWhiteSpace(name));
+
+    return name;
+}
+
+void PrintUserName()
+{
+    if (!string.IsNullOrWhiteSpace(userName)) Console.WriteLine($"{userName}!");
+}
+
+void Help()
+{
+    string helpCommandText = "Для работы с программой необходимо ввести одну из комманд: \n/start - для старта работы программы, \n/help - отображает краткую справочную информацию о том, как пользоваться программой, \n/info - предоставляет информацию о версии программы и дате её создания, \n/echo - данная команда становится доступной после ввода имени. При вводе этой команды с некоторым значением (например, /echo Hello), программа возвращает введенный текст (\"Hello\")\n/addtask - добавляет задачу в список\n/showtasks - отображает список всех добавленных задач\n/removetask - удаляет задачу по номеру в списке\n/exit - для выхода из программы";
+    Console.WriteLine(helpCommandText);
+}
+
+void Info()
+{
+    string infoCommandText = "Dерсия программы 1.0\nДата создания 23.11.2025";
+    Console.WriteLine(infoCommandText);
+}
+
+void Echo(string echoInput)
+{
+    string echoCommandText = "Имя пользователя не введено! Для работы с командой введите имя пользователя с помошью вызова команды /start";
+    Console.WriteLine(string.IsNullOrWhiteSpace(userName) ? echoCommandText : echoInput.Substring(6));
+}
+
+void CheckTaskCount()
+{
+    if (tasks.Any() && tasks.Count == taskCount) throw new TaskCountLimitException(taskCount);
+}
+
+void AddTask()
+{
+    Console.Write("Пожалуйста, введите описание задачи: ");
+    var task = Console.ReadLine();
+
+    if (IsValidTaskLength(task) && !IsDuplicateTask(task))
+    {
+        tasks.Add(task!);
+        Console.WriteLine($"Задача \"{task}\" добавлена.");
+    }
+}
+
+bool IsValidTaskLength(string? task)
+{
+    ValidateString(task);
+
+    if (task!.Length > taskLength) throw new TaskLengthLimitException(task.Length, taskLength);
+
+    return true;
+}
+
+bool IsDuplicateTask(string task)
+{
+    foreach (var t in tasks)
+    {
+        if (task.Equals(t)) throw new DuplicateTaskException(task);
+    }
+
+    return false;
+}
+
+void ShowTasks()
+{
+    if (tasks.Any())
+    {
+        Console.WriteLine("Вот ваш список задач:");
+        for (var i = 1; i <= tasks.Count(); i++)
+        {
+            Console.WriteLine($"{i} - {tasks[i - 1]}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Список задач пуст");
+    }
+}
+
+void RemoveTask()
+{
+    if (tasks.Any())
+    {
+        ShowTasks();
+        Console.Write($"Введите номер задачи для удаления: ");
+        var taskNumber = Console.ReadLine();
+        if (int.TryParse(taskNumber, out int result) && result > 0 && result <= tasks.Count())
+        {
+            tasks.Remove(tasks[result - 1]);
+            Console.WriteLine($"Задача удалена.");
+        }
+        else
+        {
+            Console.WriteLine($"Введен неверный номер для удаления");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Список задач пуст");
+    }
+}
+
+int ParseAndValidateInt(string? str, int min, int max)
+{
+    if (!(int.TryParse(str, out int number) && number >= min && number <= max)) throw new ArgumentException($"Это должно быть число от {min} до {max}");
+
+    return number;
+}
+
+void ValidateString(string? str)
+{
+    if (string.IsNullOrWhiteSpace(str)) throw new ArgumentException($"Строка не должна быть пустой");
+}
+
+void Exit()
+{
+    isExit = true;
 }
