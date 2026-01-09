bbc737aa0d4f85f8b6ccc23d24eba53dfb725911
milanaoorzhak <milanaoor@gmail.com>
2026-01-09 18:28:15 +0700

    interfaces using added

---
+++ src/GuideBot/IUserService.cs
@@ -0,0 +1,7 @@
+namespace GuideBot;
+
+public interface IUserService
+{
+    ToDoUser RegisterUser(long telegramUserId, string telegramUserName);
+    ToDoUser? GetUser(long telegramUserId);
+}
--- src/GuideBot/ToDoUser.cs
+++ src/GuideBot/ToDoUser.cs
@@ -3,12 +3,14 @@ namespace GuideBot;
 public class ToDoUser
 {
     public Guid UserId { get; set; }
+    public long TelegramUserId { get; set; }
     public string TelegramUserName { get; set; }
     public DateTime RegisteredAt { get; set; }
 
-    public ToDoUser(string telegramUserName)
+    public ToDoUser(long telegramUserId, string telegramUserName)
     {
         UserId = Guid.NewGuid();
+        TelegramUserId = telegramUserId;
         TelegramUserName = telegramUserName;
         RegisteredAt = DateTime.UtcNow;
     }
