9a6ae3ce847f09eb2d4bb42a7bf11076e45ba772
milanaoorzhak <milanaoor@gmail.com>
2026-04-20 17:27:54 +0700

    TelegramUserName updated

--- src/GuideBot/Entities/ToDoUser.cs
+++ src/GuideBot/Entities/ToDoUser.cs
@@ -4,6 +4,6 @@ public class ToDoUser
 {
     public Guid UserId { get; set; }
     public long TelegramUserId { get; set; }
-    public string? TelegramUserName { get; set; }
+    public string TelegramUserName { get; set; }
     public DateTime RegisteredAt { get; set; }
 }
